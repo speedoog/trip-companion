0d1305d5a26dad8af8da015a678645e48cf94b8b
mail.bertrand.faure@gmail.com <mail.bertrand.faure@gmail.com@e1d1f016-2be0-54ee-eed8-d0db505849b0>
2013-02-01 13:05:50 +0000

    Parser suite

--- trunk/src/com/automagic/tripcompanion/NMEAFrame.java
+++ trunk/src/com/automagic/tripcompanion/NMEAFrame.java
@@ -21,7 +21,7 @@ public class NMEAFrame {
 	// 13   = Age in seconds since last update from diff. reference station
 	// 14   = Diff. reference station ID# '*' Checksum
 
-	boolean _MinimalData;		// true if RMC frame / false if GPGGA
+	boolean _MinimalData;			// true if RMC frame / false if GPGGA
 
 	int		_TimeHour;
 	int		_TimeMinutes;
@@ -70,29 +70,44 @@ public class NMEAFrame {
 			// $GPRMC - Recommended minimum specific GPS/Transit data (12 Args)
 			//
 			// ex: 	$GPRMC,hhmmss.ss,A,llll.ll,a,yyyyy.yy,a,x.x,x.x,ddmmyy,x.x,a*hh
-			// 1    = UTC of position fix
+			
+			_MinimalData =true;
+			
+			// 1    = UTC 					"hhmmss.ss"
+			_TimeHour 	 =Integer.parseInt(sArgs[1].substring(0, 2));
+			_TimeMinutes =Integer.parseInt(sArgs[1].substring(2, 4));
+			_TimeSeconds =(int)Double.parseDouble(sArgs[1].substring(4, 10));
+
 			// 2    = Data status (V=navigation receiver warning)
-			// 3    = Latitude of fix
-			// 4    = N or S
-			// 5    = Longitude of fix
-			// 6    = E or W
+			// todo ...
+			
+			// 3    = Latitude 				"llll.ll"
+			_LatitudeValue =Double.parseDouble(sArgs[3]);
+			// 4    = LatitudeSign 			"N" or "S"
+			_LatitudeSign	=sArgs[4].charAt(0);
+			
+
+			// 5    = Longitude				"yyyyy.y"
+			_LongitudeValue =Double.parseDouble(sArgs[5]);
+			// 6    = LongitudeSign 		"E" or "W"
+			_LongitudeSign	=sArgs[6].charAt(0);
+
 			// 7    = Speed over ground in knots
 			// 8    = Track made good in degrees True
 			// 9    = UT date
 			// 10   = Magnetic variation degrees (Easterly var. subtracts from true course)
 			// 11   = E or W
 			// 12   = Checksum
-			
-			String sUTC 	=sArgs[1];
-			String sHour 	=sUTC.substring(0, 2);
-			String sMinutes	=sUTC.substring(2, 4);
-			String sSecond	=sUTC.substring(4, 10);
-			
-			_TimeHour 	 =Integer.parseInt(sHour);
-			_TimeMinutes =Integer.parseInt(sMinutes);
-			_TimeSeconds =(int)Double.parseDouble(sSecond);
-			
-			++t;
+
+			/*
+			_GPSQuality
+			_SatelliteCount
+			_HorizontalDilution
+			_AltitudeAntennaValue
+			_AltitudeAntennaUnit
+			_AltitudeGeoidalValue
+			_AltitudeGeoidalUnit
+			*/
 		}
 		else if (sHeader.compareToIgnoreCase("$GPGGA") == 0)
 		{
@@ -100,11 +115,25 @@ public class NMEAFrame {
 			// $GPGGA - Global Positioning System Fix Data. Time, position and fix related data for a GPS receiver. (14 Args)
 			//
 			// ex: 	$GPGGA,hhmmss.ss,llll.ll,a,yyyyy.yy,a,x,xx,x.x,x.x,M,x.x,M,x.x,xxxx*hh
-			// 1    = UTC of Position
-			// 2    = Latitude
-			// 3    = N or S
-			// 4    = Longitude
-			// 5    = E or W
+			//
+			
+			_MinimalData =false;
+
+			// 1    = UTC 					"hhmmss.ss"
+			_TimeHour 	 =Integer.parseInt(sArgs[1].substring(0, 2));
+			_TimeMinutes =Integer.parseInt(sArgs[1].substring(2, 4));
+			_TimeSeconds =(int)Double.parseDouble(sArgs[1].substring(4, 10));
+			
+			// 2    = Latitude 				"llll.ll"
+			_LatitudeValue =Double.parseDouble(sArgs[2]);
+			// 3    = LatitudeSign			 "N" or "S"
+			_LatitudeSign	=sArgs[3].charAt(0);
+
+			// 4    = Longitude 			"yyyyy.yy"
+			_LongitudeValue =Double.parseDouble(sArgs[4]);
+			// 5    = LongitudeSign 		"E" or "W"
+			_LongitudeSign	=sArgs[5].charAt(0);
+
 			// 6    = GPS quality indicator (0=invalid; 1=GPS fix; 2=Diff. GPS fix)
 			// 7    = Number of satellites in use [not those in view]
 			// 8    = Horizontal dilution of position
